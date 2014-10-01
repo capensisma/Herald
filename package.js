e42ceaf648ca737b4c5685367a4883c27106d9c4
Kestanous <CMTopus@me.com>
2014-10-01 11:37:27 -0400

    fixed really bad typo

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   summary: "A Universal Notifications Engine",
   version: "0.4.0",
-  git: "https://github.com/Meteor-Reaction/harold.git",
-  name: 'kestanous:harold'
+  git: "https://github.com/Meteor-Reaction/Herald.git",
+  name: 'kestanous:herald'
 });
 
 Package.onUse(function(api) {
-  
+  api.versionsFrom('METEOR@0.9.2');
   api.use(['standard-app-packages']); //TODO: reduce this to the minimum requirements.
 
   //if iron route is present add 'seen route' logic
-  api.use('iron:router', {week: true}); 
+  api.use('iron:router@0.9.0', ['server', 'client'], {week: true}); 
   //if user-status is present add online/idle logic
-  api.use('mizzao:user-status', {week: true}); 
+  api.use('mizzao:user-status@0.6.2',['server', 'client'], {week: true}); 
 
   api.addFiles('lib/notifications.js');
 
