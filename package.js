3d240f9b3e18075eafbacaad0eacd6102d7bf9f7
Kestanous <CMTopus@me.com>
2014-10-01 21:32:18 -0400

    weak no week, don't code with Siri

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A Universal Notifications Engine",
-  version: "0.5.0",
+  version: "0.5.1",
   git: "https://github.com/Meteor-Reaction/Herald.git",
   name: 'kestanous:herald'
 });
@@ -10,9 +10,9 @@ Package.onUse(function(api) {
   api.use(['standard-app-packages']); //TODO: reduce this to the minimum requirements.
 
   //if iron route is present add 'seen route' logic
-  api.use('iron:router@0.9.0', ['server', 'client'], {week: true}); 
+  api.use('iron:router@0.9.0', ['server', 'client'], {weak: true}); 
   //if user-status is present add online/idle logic
-  api.use('mizzao:user-status@0.6.2',['server', 'client'], {week: true}); 
+  api.use('mizzao:user-status@0.6.2',['server', 'client'], {weak: true}); 
 
   api.addFiles('lib/notifications.js');
 
