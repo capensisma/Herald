530f7524e3d326cc4caee8aec6d472920c63c280
Mark Lee <marklee.lhs@gmail.com>
2014-12-07 00:29:14 -0800

    Use helper function

--- server/publish.js
+++ server/publish.js
@@ -1,9 +1,7 @@
 // only publish notifications belonging to the current user
-Meteor.publish('notifications', function() {
+Meteor.publish('notifications', function () {
   var media = _.keys(Herald._clientRunners).map(function (key) {
-    var medium = {};
-    medium['media.'+key] = {send: true, sent: false};
-    return medium;
+    return Herald._setProperty('media.' + key, { send: true, sent: false });
   });
-  return Herald.collection.find({userId:this.userId, $or: media});
+  return Herald.collection.find({ userId: this.userId, $or: media });
 });
