391ab541dffb423f8d56457ae58f564dded1582b
Kestanous <CMTopus@me.com>
2015-02-09 13:46:56 -0500

    artwells:queue@0.0.4 has not been published yet

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.2');
-  api.use(['check', 'underscore', 'tracker','accounts-base', 'blaze', 'artwells:queue@0.0.4']);
+  api.use(['check', 'underscore', 'tracker','accounts-base', 'blaze', 'artwells:queue@0.0.3']);
 
   //if iron route is present add 'seen route' logic
   api.use('iron:router@0.9.3 || 1.0.0', ['server', 'client'], {weak: true});
